b95119260f256320ae29e3bd68eae08708b34b7b
jsano <jasonlee0607@gmail.com>
2023-05-24 15:04:47 -0700

    HP and 1v1

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -6,18 +6,26 @@ public class Unit : MonoBehaviour
 {
 
     private Vector3 dest;
-    [SerializeField] private float speed = 5f;
+    
     private List<Collider2D> attacking; // First is current target
-    [SerializeField] private int maxHealth;
-    private int health;
-    [SerializeField] private float atkspd;
+
+    [Header("Stats")]
+    public float speed = 5f;
+    public float maxHp;
+    private float hp;
+    private float hpbarlen = 1;
+    public float atk;
+    public float atkspd;
     private float period;
 
     // Start is called before the first frame update
     void Start()
     {
         attacking = new List<Collider2D>();
-        health = maxHealth;
+        hp = maxHp;
+        Transform bar = transform.Find("HP");
+        Vector3 temp = bar.transform.localScale;
+        bar.transform.localScale = new Vector3(hpbarlen, temp.y, temp.z);
         if (tag.CompareTo("Enemy") == 0) dest = new Vector3(-10, transform.position.y, 0); 
         else dest = new Vector3(10, transform.position.y, 0);
     }
@@ -25,38 +33,56 @@ public class Unit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (health <= 0) {
+        if (hp <= 0) {
             Destroy(gameObject);
             return;
         }
+        Transform bar = transform.Find("HP");
+        Vector3 temp = bar.transform.localScale;
+        bar.transform.localScale = new Vector3(hpbarlen * (hp/maxHp), temp.y, temp.z);
         if (attacking.Count == 0) {
             Vector3 dir = dest - transform.position;
             transform.Translate(dir.normalized*speed*Time.deltaTime);
         } else {
-            if (period > atkspd) {
-                period = 0;
-                attacking[0].GetComponent<Unit>().dealDamage(2);
-            }
-            period += UnityEngine.Time.deltaTime;    
+            Attack();
         }
 
     }
 
+    private void Attack()
+    {
+        if (period > atkspd) {
+            period = 0;
+            attacking[0].GetComponent<Unit>().receiveDamage(atk);
+        }
+        period += Time.deltaTime;
+    }
+
+    public void receiveDamage(float dmg) 
+    {
+        hp -= dmg;
+    }    
+    
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Enemy" && tag == "Unit" || other.tag == "Unit" && tag == "Enemy") {
+        if (other.tag == "Enemy" && tag == "Ally") {
             attacking.Add(other);
         }
+        if (other.tag == "Ally" && tag == "Enemy") {
+            StartCoroutine(temp(other));
+        }
+        
+    }
+
+    // Enemy will start attacking 1 frame later than ally for QOL
+    private IEnumerator temp(Collider2D other){
+        yield return new WaitForSeconds(Time.deltaTime);
+        attacking.Add(other);
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
         attacking.Remove(other);
-    }
-
-    public void dealDamage(int dmg) 
-    {
-        health -= dmg;
     }
 
 }
