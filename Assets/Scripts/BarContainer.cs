1616a1bb25bce387138f42df4ec6509932d0e09a
jsano <jasonlee0607@gmail.com>
2023-05-24 22:48:58 -0700

    BarContainer and energy bar

--- Assets/Scripts/BarContainer.cs
+++ Assets/Scripts/BarContainer.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class BarContainer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private float barLength = 1;
+    private float barWidth = 0.1f;
+    public GameObject hpBar;
+    public GameObject enBar;
+    Transform parent;
+
+    void Awake()
     {
-        
+        parent = transform.parent;
+        transform.localPosition = new Vector3(0, 0.935f, 0);
+        transform.localScale = new Vector3(barLength + 0.02f, 2*barWidth+0.05f, 0);
+        GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0.5f);
+        GetComponent<SpriteRenderer>().sortingOrder = 0;
+        makeBar(hpBar, "HP", 1);
+        makeBar(enBar, "EN", 0.87f);
+    }
+
+    private void makeBar(GameObject _bar, string name, float height)
+    {
+        //Pivot controls and is the anchor for scaling
+        GameObject pivot = new GameObject();
+        pivot.name = name + "Pivot";
+        pivot.transform.localPosition = new Vector3(-0.5f, height, 0);
+        pivot.transform.SetParent(parent, false); // Update world position
+        Transform bar = Instantiate(_bar).transform;
+        bar.transform.localPosition = new Vector3(0.5f, 0, 0);
+        bar.SetParent(pivot.transform, false);
+        if (name == "EN") {
+            bar.GetComponent<SpriteRenderer>().color = Color.magenta;
+        }
+        pivot.transform.localScale = new Vector3(0, barWidth, 0);
+        bar.GetComponent<SpriteRenderer>().sortingOrder = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Transform bar = parent.Find("HPPivot");
+        Unit g = parent.GetComponent<Unit>();
+        bar.localScale = new Vector3(barLength * (g.getCurrentHP()/g.maxHp), barWidth, 0);
         
+        Transform bar1 = parent.Find("ENPivot");
+        bar1.localScale = new Vector3(barLength * (g.getCurrentEn()/100.0f), barWidth, 0);
     }
 }
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Unit : MonoBehaviour
@@ -13,16 +14,13 @@ public class Unit : MonoBehaviour
     public float speed = 5f;
     public float maxHp;
     private float hp;
+    private float en = 0;
     public float atk;
     public float atkspd;
     private float period;
 
     [Header("Graphics")]
-    private float barLength = 1;
-    private float barWidth = 0.1f;
     public GameObject barContainer;
-    public GameObject hpBar;
-    public GameObject enBar;
     public GameObject strike;
 
     // Start is called before the first frame update
@@ -30,31 +28,9 @@ public class Unit : MonoBehaviour
     {
         attacking = new List<Collider2D>();
         hp = maxHp;
-        Transform bc = Instantiate(barContainer).transform;
-        bc.transform.position = new Vector3(0, 0.935f, 0);
-        bc.localScale = new Vector3(barLength + 0.02f, 2*barWidth+0.05f, 0);
-        bc.gameObject.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0.5f);
-        bc.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 0;
-        bc.SetParent(transform, false);
-        makeBar(hpBar, "HP", 1);
-        makeBar(enBar, "EN", 0.87f);
+        Instantiate(barContainer, transform, false);
         if (tag.CompareTo("Enemy") == 0) dest = new Vector3(-10, transform.position.y, 0); 
         else dest = new Vector3(10, transform.position.y, 0);
-    }
-
-    private void makeBar(GameObject _bar, string name, float height)
-    {
-        GameObject pivot = new GameObject();
-        pivot.name = name + "Pivot";
-        pivot.transform.position = new Vector3(-0.5f, height, 0);
-        pivot.transform.SetParent(transform, false); // Update world position
-        Transform bar = Instantiate(_bar).transform;
-        bar.transform.position = new Vector3(0.5f, 0, 0);
-        bar.SetParent(pivot.transform, false);
-        if (name == "EN")
-            bar.gameObject.GetComponent<SpriteRenderer>().color = Color.magenta;
-        bar.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 1;
-        bar.localScale = new Vector3(barLength, barWidth, 0);
     }
 
     // Update is called once per frame
@@ -64,9 +40,7 @@ public class Unit : MonoBehaviour
             Destroy(gameObject);
             return;
         }
-        Transform bar = transform.Find("HPPivot");
-        Vector3 temp = bar.localScale;
-        bar.localScale = new Vector3(barLength * (hp/maxHp), temp.y, temp.z);
+        if (hp > maxHp) hp = maxHp;
         if (attacking.Count == 0) {
             Vector3 dir = dest - transform.position;
             transform.Translate(Vector3.Scale(dir.normalized, transform.right)*speed*Time.deltaTime);
@@ -81,6 +55,7 @@ public class Unit : MonoBehaviour
         if (period > atkspd) {
             period = 0;
             attacking[0].GetComponent<Unit>().receiveDamage(atk);
+            en = Math.Min(en+25, 100);
             Instantiate(strike, transform.position + 0.5f*transform.right, transform.rotation);
         }
         period += Time.deltaTime;
@@ -89,6 +64,7 @@ public class Unit : MonoBehaviour
     public void receiveDamage(float dmg) 
     {
         hp -= dmg;
+        en = Math.Min(en+1, 100);
     }    
     
     void OnTriggerEnter2D(Collider2D other)
@@ -111,6 +87,16 @@ public class Unit : MonoBehaviour
     void OnTriggerExit2D(Collider2D other)
     {
         attacking.Remove(other);
+    }
+
+    public float getCurrentHP()
+    {
+        return hp;
+    }
+
+    public float getCurrentEn()
+    {
+        return en;
     }
 
 }
